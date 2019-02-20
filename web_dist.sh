2b20bd54f1c5651c1ae4fd6a3c4d24c5ed8d8cc5
EvanYY <evan_yangyang@163.com>
2019-02-20 12:38:01 +0800

    websocket is none domain by ws://domain:port  server and clients chart,one server to many clients

--- web_dist.sh
+++ web_dist.sh
@@ -1,4 +1,4 @@
 git pull
 git add .
-git commit -m 'window.domain: a.html width b.html has to be the same domain,PS: a.evanyy.top and b.evenyy.top'
+git commit -m 'websocket is none domain by ws://domain:port  server and clients chart,one server to many clients'
 git push
