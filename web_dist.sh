2d9ca94cd16b82ed627077bb1503f291c232c547
EvanYY <evan_yangyang@163.com>
2019-02-20 12:08:51 +0800

    window.hash: sending message by window.hash different domain a,b,c.html chart each others

--- web_dist.sh
+++ web_dist.sh
@@ -1,4 +1,4 @@
 git pull
 git add .
-git commit -m 'window.name: sending message by window.name different domain a,b,c.html chart each others'
+git commit -m 'window.hash: sending message by window.hash different domain a,b,c.html chart each others'
 git push
