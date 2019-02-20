b128ca54af89d6832286536da698dbf77b0bec4f
EvanYY <evan_yangyang@163.com>
2019-02-20 12:53:39 +0800

    nginx.conf 配置信息 location ~.*\.json   balabla gogo

--- web_dist.sh
+++ web_dist.sh
@@ -1,4 +1,4 @@
 git pull
 git add .
-git commit -m 'websocket is none domain by ws://domain:port  server and clients chart,one server to many clients'
+git commit -m 'nginx.conf 配置信息 location ~.*\.json   balabla gogo'
 git push
